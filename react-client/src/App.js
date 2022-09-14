35d5d2962a34ef39fb154d283425639b45c5ca9e
henlo <48015670+dominykase@users.noreply.github.com>
2022-09-14 17:02:45 +0300

    "create listing" button

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,23 +1,37 @@
 import './App.css';
-import { TestButton } from './Components/TestButton';
+import { CreateListingView } from './Components/Views/CreateListingView';
+import { CreateListingButton } from './Components/Buttons/CreateListingButton';
 import { Component } from 'react';
 import axios from 'axios';
 
 class App extends Component {
-    buttonId = "test-server-button";
-    buttonElement;
+    createListingButtonId = "create-listing-button";
+    createListingButton;
 
     constructor(props) {
         super(props)
         this.state = {
-            response: undefined
+            listings: undefined, // do axios request inside componentDisMount to get all listings to display
+            createListingView: false
         }
-        this.buttonElement = document.createElement('button');
+
+        this.createListingButton = document.createElement('button');
+
+        this.toggleCreateListing = this.toggleCreateListing.bind(this);
+    }
+
+    toggleCreateListing = (toggleBool) => {
+        this.setState({ createListingView: toggleBool });
     }
 
     componentDidMount() {
-        this.buttonElement = document.getElementById(this.buttonId);
+        this.createListingButton = document.getElementById(this.createListingButtonId);
 
+        this.createListingButton.addEventListener('click', () => {
+            this.toggleCreateListing(true);
+        })
+
+        /* axios example
         this.buttonElement.addEventListener('click', () => {
             axios({
                 method: 'get',
@@ -27,13 +41,29 @@ class App extends Component {
                 this.setState({ response: JSON.stringify(response.data) });
             })
         })
+        */
     }
 
     render() {
         return (
             <>
-                <TestButton />
-                <p>{this.state.response}</p>
+                {
+                    !this.state.createListingView
+                    ?
+                        <div className="listings-container">
+                            <>
+                                <CreateListingButton
+                                    id="create-listing-button"
+                                    text="New listing"
+                                    class="btn"
+                                />
+                                {/* map all the listings here */}
+                            </>
+                        </div>
+                    :
+                        <CreateListingView />
+                }
+                
             </>
         );
     } 
---
+++ react-client/src/Components/Views/CreateListingView.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+export class CreateListingView extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="create-listing-form">
+                {"input form goes gere"}
+            </div>
+        );
+    }
+}
