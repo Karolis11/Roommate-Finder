a25eee0a0564fe187ddfc11039d6109215e87076
henlo <48015670+dominykase@users.noreply.github.com>
2022-09-24 17:21:36 +0300

    cleaned up code (spacing, brackets)

--- roommate-app/Controllers/ListingController.cs
+++ roommate-app/Controllers/ListingController.cs
@@ -1,51 +1,44 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using roommate_app.Models;
 using System.Text.Json;
-using System.Text.Json.Serialization;
-using Newtonsoft.Json;
 
-namespace roommate_app.Controllers
+namespace roommate_app.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ListingController : Controller
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class ListingController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public ListingController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
+        _logger = logger;
+    }
 
-        public ListingController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+    private List<Listing> LoadJson()
+    {
+        using StreamReader r = new StreamReader("./Data/listings.json");
+        string json = r.ReadToEnd();
+        List<Listing> listings = JsonSerializer.Deserialize<List<Listing>>(json);
+        return listings;
+      
+    }
 
-        public List<Listing> LoadJson()
-        {
-            using (StreamReader r = new StreamReader("./Data/listings.json"))
-            {
-                string json = r.ReadToEnd();
-                List<Listing> listings = JsonConvert.DeserializeObject<List<Listing>>(json);
-                return listings;
-            }
-        }
+    [HttpGet]
+    public JsonResult Get()
+    {
+        return Json(this.LoadJson());
+    }
 
-        [HttpGet]
-        public JsonResult Get()
-        {
-            //string[] lines = System.IO.File.ReadAllLines(@"../react-client/src/data/failas.txt");
-            return Json(this.LoadJson());
-        }
-
-        [HttpPost]
-        public JsonResult Submit([FromBody] Listing listing)
-        {
-            List<Listing> existingListings = LoadJson();
-            existingListings.Add(listing);
-            string json = Newtonsoft.Json.JsonConvert.SerializeObject(existingListings);
-            System.IO.StreamWriter tsw = new System.IO.StreamWriter("Data/listings.json", false);
-            tsw.WriteLine(json);
-            //tsw.WriteLine('\n');
-            tsw.Close();
-            return Json(existingListings);
-        }
+    [HttpPost]
+    public JsonResult Submit([FromBody] Listing listing)
+    {
+        List<Listing> existingListings = LoadJson();
+        existingListings.Add(listing);
+        string json = JsonSerializer.Serialize(existingListings);
+        StreamWriter tsw = new StreamWriter("Data/listings.json", false);
+        tsw.WriteLine(json);
+        tsw.Close();
+        return Json(existingListings);
     }
 }
