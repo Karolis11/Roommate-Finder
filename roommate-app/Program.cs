3a9389757463434cf20e349f4ec84024bbfaec82
Karolis Janušonis <90441990+Karolis11@users.noreply.github.com>
2022-09-24 18:00:42 +0300

    commit test

--- roommate-app/Program.cs
+++ roommate-app/Program.cs
@@ -39,7 +39,9 @@ else
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
+//test
+//test
+//test
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
