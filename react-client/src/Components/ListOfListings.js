b5e8d41b2fa7a1cf10bceba1ec0367ea26b9f842
Karolis Janušonis <90441990+Karolis11@users.noreply.github.com>
2022-10-10 21:29:23 +0300

    fix comments

--- react-client/src/Components/ListOfListings.js
+++ react-client/src/Components/ListOfListings.js
@@ -5,13 +5,6 @@ export class ListOfListings extends Component {
 
     constructor(props) {
         super(props);
-
-        // var today = new Date(),
-        // date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
-
-        // this.state = {
-        //     date: date
-        // };
     }
 
     render() {
