8bc8fede60035eff702f558b7008240dd5ac2d16
Edvinas Kumža <edkumza@gmail.com>
2022-11-08 23:45:44 +0200

    Entry Screen CSS

--- react-client/src/Components/Pages/EntryScreen.js
+++ react-client/src/Components/Pages/EntryScreen.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import img1 from './Icons/RoommateLogo.png';
+import './EntryScreen.css';
 
 export const EntryScreen = (props) => {
     const navigate = useNavigate();
 
     return (
         <div className="login-signup-btn-container">
-            <button
+            <img className="entry-logo" src={img1}></img>
+            <h2>Roommate Finder</h2>
+            <button className="entry-btn"
                 id="loginButton"
                 onClick={() => { navigate(`/login`); }}
             >
                 Login
             </button>
-            <button
+            <button className="entry-btn"
                 id="signupButton"
                 onClick={() => { navigate(`/signup`); }}
             >
