42eb3c34da6ec39fa69a0142070699e79ed1c6f3
TisIDom <dom20120620@gmail.com>
2022-11-13 15:49:49 +0200

    Added a generic service

--- roommate-app/Controllers/ListingController.cs
+++ roommate-app/Controllers/ListingController.cs
@@ -16,29 +16,26 @@ public class ListingController : Controller
     private readonly ILogger<HomeController> _logger;
     private readonly IListingCompreterFactory _listingFactory;
     private readonly IErrorLogging _errorLogging;
-    private readonly IListingService _listingService;
-    private readonly IUserService _userService;
+    private readonly IGenericService _genericService;
 
     public ListingController(
         ILogger<HomeController> logger, 
-        IListingCompreterFactory listingFactory,
-        IListingService listingService, 
+        IListingCompreterFactory listingFactory, 
         IErrorLogging errorLogging,
-        IUserService userService
+        IGenericService genericService
         )
     {
         _logger = logger;
         _listingFactory = listingFactory;
-        _listingService = listingService;
         _errorLogging = errorLogging;
-        _userService = userService;
+        _genericService = genericService;
     }
 
     [HttpGet]
     [Route("sort")]
     public async Task<ActionResult> GetSortedListings(SortMode sort, string city)
     {
-        var existingListings = await _listingService.GetAllAsync();
+        var existingListings = await _genericService.GetAllAsync<Listing>();
 
         var factory = _listingFactory.createListingComparerFactory();
         var comparer = factory.GetComparer(sortMode: sort, city: city);
@@ -52,7 +49,7 @@ public class ListingController : Controller
     public async Task<ActionResult> Submit([FromBody] Listing listing)
     {
         List<Listing> existingListings = new List<Listing>();
-        List<User> existingUsers = await _userService.GetAllAsync();
+        List<User> existingUsers = await _genericService.GetAllAsync<User>();
         listing.Date = DateTime.Now.ToString("yyyy-MM-dd");
         User user = existingUsers.Where(u => u.Email == listing.Email).First();
 
@@ -61,7 +58,7 @@ public class ListingController : Controller
 
         try
         {
-            await _listingService.AddAsync(listing);
+            await _genericService.AddAsync<Listing>(listing);
             existingListings.Add(listing);
         }
         catch(ArgumentNullException e){
@@ -77,7 +74,7 @@ public class ListingController : Controller
             _errorLogging.messageError("Unexpected error, please restart the program");
         }
 
-        return base.Ok(await _listingService.GetAllAsync());
+        return base.Ok(await _genericService.GetAllAsync<Listing>());
     }
 
     [HttpPost]
@@ -86,7 +83,7 @@ public class ListingController : Controller
     {
         try
         {
-            await _listingService.UpdateAsync(listing.Id, listing);
+            await _genericService.UpdateAsync<Listing>(listing.Id, listing);
         } 
         catch(SqlException e)
         {
