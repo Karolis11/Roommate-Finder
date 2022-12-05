85d8239b4a065065deac31313eb83e116a845741
henlo <48015670+dominykase@users.noreply.github.com>
2022-12-05 16:38:08 +0200

    added interceptor that measures computation time in services

---
+++ roommate-app/Interceptors/DurationInterceptor.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using Castle.DynamicProxy;
+using roommate_app.Other.Logger;
+
+namespace roommate_app.Interceptors;
+public class DurationInterceptor : IInterceptor
+{
+    private readonly IDurationLogger _logger;
+
+    DurationInterceptor(IDurationLogger logger)
+    {
+        _logger = logger;
+    }
+
+    public void Intercept(IInvocation invocation)
+    {
+        var sw = Stopwatch.StartNew();
+        try
+        {
+            invocation.Proceed();
+        } finally
+        {
+            sw.Stop();
+            _logger.Log(
+                String.Format(
+                    "{0} took {1}ms", 
+                    invocation.Method.Name,
+                    sw.ElapsedMilliseconds
+                )
+            );
+        }
+    }
+}
+
