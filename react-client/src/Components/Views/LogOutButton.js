1d32e39aa00a61d5199c95a4f3027c51022f4aa0
TisIDom <dom20120620@gmail.com>
2022-12-06 15:52:16 +0200

    Added logout button with no functionality

---
+++ react-client/src/Components/Views/LogOutButton.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export class LogOutButton extends React.Component {
+
+   constructor(props) {
+    super(props);
+    this.state = {
+      disabled: !localStorage.getItem("token"),
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+//    event.preventDefault();
+//    localStorage.setItem("token", "");
+//    this.setState({ disabled: true });
+//    window.location.reload(false);
+  }
+
+  render() {
+    return (
+      <button
+        class= "button-top-container"
+        onClick={this.handleClick}
+        disabled={this.state.disabled}
+      >
+        Log out
+      </button>
+    );
+  }
+}
+export default LogOutButton;
